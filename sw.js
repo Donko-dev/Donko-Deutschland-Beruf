cb5632bece834d9a451f1b06204e897120397309
Hassane DONKO <empiredonko@gmail.com>
2026-08-05 08:40:20 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    externes (Tailwind CDN, Google Fonts) au premier chargement, pour un
    fonctionnement 100% hors ligne dès la deuxième visite. */
 
-const CACHE_NAME = 'donko-deutschland-beruf-v9';
+const CACHE_NAME = 'donko-deutschland-beruf-v10';
 
 const PRECACHE_URLS = [
   './',
